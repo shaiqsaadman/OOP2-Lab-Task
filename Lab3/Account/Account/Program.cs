a2742bf8244928a75e288a77e61484a71bd9ea1e
shaiqsaadman <igithubu@619>
2020-10-31 11:45:58 +0600

    Account

---
+++ Lab3/Account/Account/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Account
+{
+    
+    
+        class Program
+        {
+            private string accName { get; set; }
+            private string acid;
+            private int balance;
+            static void Main(string[] args)
+            {
+                Program p = new Program();
+                p.accName ="hello";
+                p.acid = 10;
+                p.balance = 10;
+
+
+            int m = p.accName;
+            int n = p.acid;
+            int o = p.balance;
+
+            Console.WriteLine(m);
+                Console.WriteLine(n);
+            Console.WriteLine(o);
+
+            Console.ReadLine();
+            }
+        }
+    
+}
