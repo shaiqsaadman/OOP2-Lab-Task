a6167218547f9af4be29aabb134638fd59d56eea
shaiqsaadman <igithubu@619>
2020-10-31 11:41:22 +0600

    Course

---
+++ Lab3/Course/Course/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Course
+{
+    
+    
+        class Program
+        {
+            private string courseName { get; set; }
+            private string courseCode;
+            private int courseCredit;
+
+            static void Main(string[] args)
+            {
+                Program p = new Program();
+                p.courseName = "abc";
+                p.courseCode = 10;
+                p.courseCredit = 10;
+
+                int m = p.courseName;
+                int n = p.courseCode;
+                int o = p.courseCredit;
+
+                Console.WriteLine(m);
+                Console.WriteLine(n);
+                Console.WriteLine(o);
+
+            Console.ReadLine();
+            }
+        }
+    
+}
