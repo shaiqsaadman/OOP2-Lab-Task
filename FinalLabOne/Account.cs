823853ca455d0d16243bd235a4158ffd5e37c63f
shaiqsaadman <igithubu@619>
2020-11-23 11:11:16 +0600

    Final term lab 1
    Lab 1

---
+++ FinalLabOne/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace FinalLAbOne
+{
+    abstract class Account
+    {
+        
+        
+        public Account(string accName, string acId, double balance)
+        {
+            AccountHolderName = accName;
+            AccId= acId;
+            Balance = balance;
+        }
+        public void Deposit(int amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine("Deposite amount : " + amount);
+            }
+        }
+        abstract public void Withdraw(int amount);
+        
+        public void Transfer(int amount, Account receiver)
+        {
+            if (amount < Balance)
+            {
+                Balance -= amount;
+                receiver.Balance += amount;
+            }
+            else
+                Console.WriteLine("Transection failed .");
+
+        }
+        abstract public void ShowInfo();
+
+    }
+}
---
+++ FinalLabOne/Savings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace FinalLabOne
+{
+    class Savings:Account
+    {
+        
+        
+        public Savings(String accName,string accId,double balance):base(accName,accId,balance)
+        {
+            this.limitOfMonthlyTransaction = 5;
+        }
+
+        public override void Withdraw(int amount)
+        {
+            if (amount <= (Balance - 500))
+            {
+                Balance -= amount;
+            }
+            else
+                Console.WriteLine("Withdraw failed.");
+        }
+
+       public override void ShowInfo()
+        {
+            
+            Console.WriteLine("Account ID : " + AccId);
+            Console.WriteLine("Balance : " + Balance);
+
+        }
+    }
+}
