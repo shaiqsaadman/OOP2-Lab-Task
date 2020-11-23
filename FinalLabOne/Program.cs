332023e03a62dfea0a2cedc13cab44f2270099df
shaiq-dev <62259757+shaiqsaadman@users.noreply.github.com>
2020-11-23 11:15:55 +0600

    Lab 1 final
    final

---
+++ FinalLabOne/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace FinalLabOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Savings("Shaiq", 1000);
+            a1.ShowInfo();
+            a1.Deposit(100);
+            a1.Withdraw(300);
+            a1.ShowInfo();
+            Console.WriteLine();
+          
+            Account a2 = new Fixed("Shaiq", 1000);
+            a2.ShowInfo();
+            a2.Deposit(100);
+            a2.Withdraw(300);
+            a2.ShowInfo();
+            
+        }
+    }
+}
