a757a4fd7f54f20b7fa4b817f36d302c361508e3
shaiq-dev <62259757+shaiqsaadman@users.noreply.github.com>
2020-12-07 11:38:50 +0600

    Update ScientificCalculatorInterface.cs

--- Final_Lab2/Final_Lab2/ScientificCalculatorInterface.cs
+++ Final_Lab2/Final_Lab2/ScientificCalculatorInterface.cs
@@ -4,11 +4,27 @@ using System.Text;
 
 namespace Final_Lab2
 {
-    interface ScientificCalculatorInterface
+    class ScientificCalculator : IScientificCalculatorInterface
     {
-        int XtoY(int x, int y);
+        public ScientificCalculator()
+        {
+            Console.WriteLine("Scientific calculator.");
+        }
+        
+        public double SquareRoot(double x)
+        {
+            Console.WriteLine("Square Root : "+ Math.Sqrt(x));
+            return Math.Sqrt(x);
+        }
 
-        int factorial(int x);
+        public int XtoY(int x, int y)
+        {
+            Console.WriteLine("X To Y : " + Convert.ToInt32(Math.Pow(x, y)));
+            return Convert.ToInt32(Math.Pow(x, y));
+        }
+
+       
+
 
     }
 }
